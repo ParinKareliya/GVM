999705e76b2fda752025af350d2030c6fae9e2a4
ParinKareliya <kparin59@gmail.com>
2024-11-27 00:18:47 +0530

    Add Readme Text File and Add Responsive and Sidebar

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,10 @@
- 
+$(".menu").click(function(){
+    $(".mobile").toggleClass("open");
+    $(".backdrop").toggleClass("open");
+
+
+    $(".backdrop").click(function(){
+        $(".mobile").removeClass("open");
+        $(".backdrop").removeClass("open");
+    });
+});
