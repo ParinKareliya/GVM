aabffdd7e8c4b2317ae2b22abe4be27160557673
ParinKareliya <kparin59@gmail.com>
2024-11-27 23:02:02 +0530

    Slider Sidebar

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,12 @@
 $(".menu").click(function(){
     $(".mobile").toggleClass("open");
     $(".backdrop").toggleClass("open");
+    $("body").toggleClass("open");
 
 
     $(".backdrop").click(function(){
         $(".mobile").removeClass("open");
         $(".backdrop").removeClass("open");
+        $("body").removeClass("open");
     });
 });
